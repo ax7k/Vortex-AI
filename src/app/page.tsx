bfc20f838ce8c00466a9eb803d44d65cfd23af9e
ax7k <ax7kram@gmail.com>
2025-10-15 21:51:34 +0400

    database_integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
-import { Button } from "@/components/ui/button";
-
 const Page = () => {
 	return (
-		<div className="font-bold">
+		<div>
 			<h1>Welcome to the Home Page</h1>
-			<Button>
-				Click Me
-			</Button>
 		</div>
 	);
 }
