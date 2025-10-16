c3de2f55c99992cfe417af6d3478f0b7312f181d
ax7k <ax7kram@gmail.com>
2025-10-16 15:07:36 +0400

    stage_3-trpc_setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,39 @@
-const Page = () => {
+import { trpc, getQueryClient } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+import { Client } from "./client";
+
+const Page = async () => {
+	const queryClient = getQueryClient();
+	void queryClient.prefetchQuery(trpc.craete_AI.queryOptions({ text: "badaboom" }));
 	return (
-		<div>
-			<h1>Welcome to the Home Page</h1>
-		</div>
+		<HydrationBoundary state={dehydrate(queryClient)}>
+			<Suspense fallback={<p>Loading...</p>}>
+				<Client />
+			</Suspense>
+		</HydrationBoundary>
 	);
 }
  
 export default Page;
+
+
+
+
+// "use client";
+
+// import { useTRPC } from "@/trpc/client";
+// import { useQuery } from "@tanstack/react-query";
+
+// const Page = () => {
+// 	const trpc = useTRPC();
+// 	const { data } = useQuery(trpc.craete_AI.queryOptions({ text: "badaboom" }));
+
+// 	return (
+// 		<div>
+// 			{JSON.stringify(data)}
+// 		</div>
+// 	);
+// }
+ 
+// export default Page;
