bfb5210da4104c395d7135d07a8a316b0a3db84f
ax7k <ax7kram@gmail.com>
2025-10-18 13:36:48 +0400

    implemented logic for projects creation and routing

--- src/modules/messages/server/procedures.ts
+++ src/modules/messages/server/procedures.ts
@@ -15,12 +15,19 @@ export const messagesRouter = createTRPCRouter({
   create: baseProcedure
     .input(
       z.object({
-        value: z.string().min(1, { message: "Message cannot be empty" }),
+        value: z
+          .string()
+          .min(1, { message: "Message cannot be empty" })
+          .max(10000, { message: "Message is too long" }),
+        projectId: z
+          .string()
+          .min(1, { message: "Project ID is required" }),
       })
     )
     .mutation(async ({ input }) => {
       const createdMessage = await prisma.message.create({
         data: {
+          projectId: input.projectId,
           content: input.value,
           role: "USER",
           type: "RESULT",
@@ -30,6 +37,7 @@ export const messagesRouter = createTRPCRouter({
         name: "code-agent/run",
         data: {
           value: input.value,
+		  projectId: input.projectId,
         },
       });
       return createdMessage;
