b632706f367d2ed527f154012cb33ca4f9e7303e
ax7k <ax7kram@gmail.com>
2025-10-19 12:25:59 +0400

    project header

---
+++ src/modules/projects/ui/components/project-header.tsx
@@ -0,0 +1,101 @@
+import Link from "next/link";
+import Image from "next/image";
+import { useTheme } from "next-themes";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { ChevronDownIcon, ChevronLeftIcon, SunMoonIcon } from "lucide-react";
+import { useTRPC } from "@/trpc/client";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuPortal,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
+
+interface Props {
+  projectId: string;
+}
+export const ProjectHeader = ({ projectId }: Props) => {
+  const trpc = useTRPC();
+  const { data: project } = useSuspenseQuery(
+    trpc.projects.getOne.queryOptions({ id: projectId })
+  );
+
+  const { setTheme, theme } = useTheme();
+
+  return (
+    <header className="p-2 flex justify-between items-center border-b">
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="ghost"
+            size="sm"
+            className="focus-visible:ring-0 hover:bg-transparent hover:opacity-75 transition-opacity pl-2!"
+          >
+            <Image
+              src="/vortex_logo.svg"
+              alt="Vortex logo"
+              width={18}
+              height={18}
+              className="shrink-0"
+            />
+            <span className="text-sm font-medium">{project.name}</span>
+            <ChevronDownIcon />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent
+          side="bottom"
+          align="start"
+          className="border-2 rounded p-1 bg-gray-50 dark:bg-gray-700 flex flex-col gap-1"
+        >
+          <DropdownMenuItem
+            asChild
+            className="flex flex-row bg-white dark:bg-gray-800 p-2 rounded hover:bg-gray-100 border"
+          >
+            <Link href="/">
+              <ChevronLeftIcon className="text-muted-foreground" />
+              <span>Go to dashboard</span>
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuSub>
+            <DropdownMenuSubTrigger className="gap-2 flex flex-row bg-white dark:bg-gray-800 p-2 rounded hover:bg-gray-100 border">
+              <SunMoonIcon className="pt-1 size-5 text-muted-foreground" />
+              <span>Appearance</span>
+            </DropdownMenuSubTrigger>
+            <DropdownMenuPortal>
+              <DropdownMenuSubContent>
+                <DropdownMenuRadioGroup value={theme} onValueChange={setTheme} className="border-2 rounded ms-2 p-0.5 bg-gray-50 dark:bg-gray-700 flex flex-col gap-0.5">
+                  <DropdownMenuRadioItem
+                    value="light"
+                    className="bg-white dark:bg-gray-800 p-1 rouded "
+                  >
+                    <span>Light</span>
+                  </DropdownMenuRadioItem>
+                  <DropdownMenuRadioItem
+                    value="dark"
+                    className="bg-white dark:bg-gray-800 p-1 rouded "
+                  >
+                    <span>Dark</span>
+                  </DropdownMenuRadioItem>
+                  <DropdownMenuRadioItem
+                    value="system"
+                    className="bg-white dark:bg-gray-800 p-1 rouded "
+                  >
+                    <span>System</span>
+                  </DropdownMenuRadioItem>
+                </DropdownMenuRadioGroup>
+              </DropdownMenuSubContent>
+            </DropdownMenuPortal>
+          </DropdownMenuSub>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </header>
+  );
+};
